3865da15b282b1b9276c4ee6f6d6d3f6b3075d60
urvi-occ <urvi@occ-data.org>
2024-07-17 18:22:05 -0500

    removed OCCRegexDetector

---
+++ src/js/instances.js
@@ -0,0 +1,51 @@
+const instances = {
+    "bloodpac": {
+        "logo_link": "https://data.bloodpac.org",
+        "dictionary_endpoint": "https://data.bloodpac.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://data.bloodpac.org/index/_stats",
+        "subject_stats_endpoint": "https://data.bloodpac.org/api/search/datasets?nodes=case",
+        "file_count": 35662,
+        "total_file_size": 34824758954808,
+        "subject_count": 4441
+    },
+    "canine": {
+        "logo_link": "https://caninedc.org",
+        "dictionary_endpoint": "https://caninedc.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://caninedc.org/index/_stats",
+        "file_count": 3820,
+        "total_file_size": 1884253865578,
+        "subject_count": 1499
+    },
+    "covid19": {
+        "logo_link": "https://chicagoland.pandemicresponsecommons.org",
+        "dictionary_endpoint": "https://chicagoland.pandemicresponsecommons.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://chicagoland.pandemicresponsecommons.org/index/_stats",
+        "subject_stats_endpoint": "https://chicagoland.pandemicresponsecommons.org/api/search/datasets?nodes=subject",
+        "file_count": 285849,
+        "total_file_size": 117637133283369,
+        "subject_count": 320124
+    },
+    "va": {
+        "logo_link": "https://va.data-commons.org/",
+        "dictionary_endpoint": "https://va.data-commons.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://va.data-commons.org/index/_stats",
+        "file_count": 11494,
+        "total_file_size": 1870677828957,
+        "subject_count": 648242
+    },
+    "vpodc": {
+        "logo_link": "https://vpodc.data-commons.org",
+        "dictionary_endpoint": "https://vpodc.data-commons.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://vpodc.data-commons.org/index/_stats",
+        "file_count": 352786,
+        "total_file_size": 2184859714735,
+        "subject_count": 163695
+    },
+    "edc": {
+        "logo_link": "https://portal.occ-data.org",
+        "dictionary_endpoint": "https://portal.occ-data.org/api/v0/submission/_dictionary/_all",
+        "file_stats_endpoint": "https://portal.occ-data.org/index/_stats",
+        "file_count": 33441289,
+        "total_file_size": 99197838516274
+    }
+}
