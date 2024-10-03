5bbced7ec2c3e5f9ff2ce8da7667819d892d763e
Mikisha10 <Mikisha10@users.noreply.github.com>
2024-10-03 05:10:08 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -29,8 +29,8 @@ const instances = {
         "logo_link": "https://va.data-commons.org/",
         "dictionary_endpoint": "https://va.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://va.data-commons.org/index/_stats",
-        "file_count": 17199,
-        "total_file_size": 2957459599903,
+        "file_count": 17214,
+        "total_file_size": 2959776108193,
         "subject_count": 648242
     },
     "vpodc": {
