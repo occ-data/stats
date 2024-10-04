cf2aad54224e065c7f8cd753a922950211a9c32e
Mikisha10 <Mikisha10@users.noreply.github.com>
2024-10-04 05:10:05 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -4,8 +4,8 @@ const instances = {
         "dictionary_endpoint": "https://data.bloodpac.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.bloodpac.org/index/_stats",
         "subject_stats_endpoint": "https://data.bloodpac.org/api/search/datasets?nodes=case",
-        "file_count": 36298,
-        "total_file_size": 40646264919713,
+        "file_count": 36612,
+        "total_file_size": 40933271765096,
         "subject_count": 4441
     },
     "canine": {
@@ -29,8 +29,8 @@ const instances = {
         "logo_link": "https://va.data-commons.org/",
         "dictionary_endpoint": "https://va.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://va.data-commons.org/index/_stats",
-        "file_count": 17214,
-        "total_file_size": 2959776108193,
+        "file_count": 17304,
+        "total_file_size": 2977054078751,
         "subject_count": 648242
     },
     "vpodc": {
