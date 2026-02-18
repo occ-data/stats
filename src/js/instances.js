1af77ac0ee420cd5b515e4ebcab4218e4ebcfcbb
Mikisha10 <86422072+Mikisha10@users.noreply.github.com>
2026-02-18 05:53:35 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -4,7 +4,7 @@ const instances = {
         "dictionary_endpoint": "https://data.bloodpac.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://data.bloodpac.org/index/_stats",
         "subject_stats_endpoint": "https://data.bloodpac.org/api/search/datasets?nodes=case",
-        "file_count": 36689,
+        "file_count": 36694,
         "total_file_size": 41502923045181,
         "subject_count": 4466
     },
