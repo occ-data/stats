aeca759353e1163ae84c3c7a88755340da10598c
Mikisha10 <86422072+Mikisha10@users.noreply.github.com>
2026-03-28 05:49:11 +0000

    Automated Stats Update [skip-counts-update]

--- src/js/instances.js
+++ src/js/instances.js
@@ -29,8 +29,8 @@ const instances = {
         "logo_link": "https://vpodc.data-commons.org",
         "dictionary_endpoint": "https://vpodc.data-commons.org/api/v0/submission/_dictionary/_all",
         "file_stats_endpoint": "https://vpodc.data-commons.org/index/_stats",
-        "file_count": 424893,
-        "total_file_size": 2257953638241,
+        "file_count": 424896,
+        "total_file_size": 2257956419608,
         "subject_count": 163695
     }
 }
